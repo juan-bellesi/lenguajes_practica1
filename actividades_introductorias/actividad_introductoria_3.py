79a01d349338e6659979a171f60cfdc540933c66
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 11:57:25 -0300

    primer uso del bucle for usando una lista con los factores

--- actividades_introductorias/actividad_introductoria_3.py
+++ actividades_introductorias/actividad_introductoria_3.py
@@ -1,4 +1,13 @@
 #Crea un programa que solicite al usuario un número
 # y muestre su tabla de multiplicar del 1 al 10 utilizando un bucle for.
+factores = [1,2,3,4,5,6,7,8,9,10]
 
 numero = int(input("Ingrese un numero entero..."))
+print(f"El numero ingresado es {numero}.")
+
+for factor in factores:
+    multiplicacion = numero * factor
+    print (multiplicacion)
+    
+    
+    
