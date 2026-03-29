2238c385fd29db9cd7756ea9f43c5ddbe82f51ae
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 12:19:26 -0300

    mismo ejercicio, pero con range

--- actividades_introductorias/actividad_introductoria_3.py
+++ actividades_introductorias/actividad_introductoria_3.py
@@ -5,9 +5,12 @@ factores = [1,2,3,4,5,6,7,8,9,10]
 numero = int(input("Ingrese un numero entero..."))
 print(f"El numero ingresado es {numero}.")
 
-for factor in factores:
-    multiplicacion = numero * factor
-    print (multiplicacion)
+#for factor in factores:
+ #   multiplicacion = numero * factor
+  #  print (multiplicacion)
     
-    
+#ahora misma idea, pero con range
+
+for i in range(1,11):    
+    print (f"{numero} x {i} = {numero*i}")
     
