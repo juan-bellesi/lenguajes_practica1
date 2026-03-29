33e9ed9c7a8fce69caea23d78094d1553a8855ea
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 16:51:26 -0300

    ahora use el break con while true...

--- actividades_introductorias/actividad_introductoria_5.py
+++ actividades_introductorias/actividad_introductoria_5.py
@@ -7,8 +7,15 @@ sumatoria = 0
 
 precio = float(input("Ingrese el precio del producto y escriba 0 cuando quiera dar por terminada la suma: "))
 
-while precio != 0:
-    sumatoria = sumatoria + precio
-    precio = float(input("Ingrese otro precio. Recuerde usar cero para finalizar: "))
+#while precio != 0:
+#    sumatoria = sumatoria + precio
+#   precio = float(input("Ingrese otro precio. Recuerde usar cero para finalizar: "))
+
+while True:
+    sumatoria += precio
+    precio = float(input("Ingrese otro numero. Recuerde usar cero para finalizar: "))
+    if precio == 0:
+        break
 
 print(f"El precio total de los productos ingresados es {sumatoria} pesos")
+
