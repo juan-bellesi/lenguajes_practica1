97fbc65bca1a2ccdd0132286012cf66c17949c5c
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 16:04:29 -0300

    imprimo los numeros desde 1 hasta el n ingresado por teclado

---
+++ actividades_introductorias/actividad_introductoria_4.py
@@ -0,0 +1,10 @@
+#Dado un número N ingresado por el usuario
+# imprima los números del 1 al N pero saltee los múltiplos de 5.
+# Nota: utilizá la sentencia continue donde haga falta.
+
+numero = int(input("Ingrese un numero..."))
+
+for i in range(1,numero+1):
+    print(i)
+        
+    
