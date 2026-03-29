6e345f80cdaa60824f70358c5b3c7cb9296a533a
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 17:09:17 -0300

    primera incursion en listas y su recorrido

---
+++ actividades_introductorias/actividad_introductoria_6.py
@@ -0,0 +1,22 @@
+#Modifica el ejercicio 4 para que, en lugar de imprimir los números, genere dos listas:
+#una con los múltiplos de 5 y otra con el resto de los números.
+# Imprimí ambas listas al finalizar.
+
+lista_multiplos = []
+otra_lista = []
+
+numero = int(input("Ingrese un numero..."))
+
+for i in range(1,numero+1):
+    if i % 5 == 0:
+        lista_multiplos.append(i)
+    else:
+        otra_lista.append(i)
+        
+for j in range(len(lista_multiplos)):
+    print(lista_multiplos[j])
+
+for k in range(len(otra_lista)):
+    print(otra_lista[k])
+
+    
