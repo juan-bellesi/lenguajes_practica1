e6eddf650d07ea373035d34efbfd5d0b46e34acd
Juan Bellesi <jmbellesi94@gmail.com>
2026-03-29 16:17:46 -0300

    aca lo hice con un bucle while para probar

--- actividades_introductorias/actividad_introductoria_4.py
+++ actividades_introductorias/actividad_introductoria_4.py
@@ -3,9 +3,19 @@
 # Nota: utilizá la sentencia continue donde haga falta.
 
 numero = int(input("Ingrese un numero..."))
+i = 1
+# for i in range(1,numero+1):
+#    if i % 5 == 0:
+#        continue
+#    print(i)
+    
+# Misma consigna, pero usando while., pa' practicá' nomá'
 
-for i in range(1,numero+1):
+while i <= numero:
     if i % 5 == 0:
+        i+= 1
         continue
     print(i)
+    i+=1
+
     
